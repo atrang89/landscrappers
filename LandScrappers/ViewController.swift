b50b940551939a0f89bb85a152492b5e2e69506b
atrang789 <andrew.trang.thanh@gmail.com>
2016-10-27 21:21:08 -0500

    Initial Commit

---
+++ LandScrappers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LandScrappers
+//
+//  Created by Andy Trang on 10/27/16.
+//  Copyright © 2016 AlwaysOnAlpha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
