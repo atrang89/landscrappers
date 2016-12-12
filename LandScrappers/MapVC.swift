2748283a4085042e1b7da35a99a087628de875a0
atrang789 <andrew.trang.thanh@gmail.com>
2016-12-11 23:09:24 -0600

    converted address to lon and lat

--- LandScrappers/MapVC.swift
+++ LandScrappers/MapVC.swift
@@ -9,15 +9,43 @@
 import UIKit
 import MapKit
 
-class MapVC: UIViewController, MKMapViewDelegate {
+class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var map: MKMapView!
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let latitude: CLLocationDegrees = 44.5644118
         
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+//        let latitude: CLLocationDegrees = 44.5644118
+//        let longitude: CLLocationDegrees = -88.109463
+//        
+//        //Map scaling
+//        let latDelta: CLLocationDegrees = 0.05
+//        let lonDelta: CLLocationDegrees = 0.05
+//        
+//        //Overall map view using latDelta and lonDelta
+//        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+//        
+//        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+//        
+//        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+//        
+//        map.setRegion(region, animated: true)
+//        
+//        let annotation = MKPointAnnotation()
+//        annotation.title = "My House"
+//        annotation.subtitle = "No one comes into my house without paying the price"
+//        annotation.coordinate = location
+//        map.addAnnotation(annotation)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let latitude: CLLocationDegrees = 44.5644118
         let longitude: CLLocationDegrees = -88.109463
         
         //Map scaling
@@ -31,7 +59,7 @@ class MapVC: UIViewController, MKMapViewDelegate {
         
         let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
         
-        map.setRegion(region, animated: true)
+        self.map.setRegion(region, animated: true)
         
         let annotation = MKPointAnnotation()
         annotation.title = "My House"
--- LandScrappers/RegisterVC.swift
+++ LandScrappers/RegisterVC.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import CoreLocation
 
 class RegisterVC: UIViewController {
 
+    @IBOutlet weak var address: UITextField!
+    @IBOutlet weak var coordinates: UILabel!
+    @IBOutlet weak var coordinatesLon: UILabel!
+    
+    var geoCoder: CLGeocoder?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.geoCoder = CLGeocoder()
+    }
+    
+    @IBAction func geoCode(_ sender: AnyObject) {
+        if let text = self.address.text
+        {
+            self.geoCoder?.geocodeAddressString(text, completionHandler: { (placemarks, error) in
+                if error != nil
+                {
+                    print("Address: \(error)")
+                } else {
+                    if let placemarks = placemarks?.last {
+                        self.coordinates.text = "lat: \(placemarks.location!.coordinate.latitude)"
+                        self.coordinatesLon.text = "long: \(placemarks.location!.coordinate.longitude)"
+                    }
+                }
+            })
+        }
+
     }
 }
